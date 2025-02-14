e9273c1c063230fd44f8416a0870a38b3e9aeef8
AjitSonar <148670404+AjitSonar@users.noreply.github.com>
2025-02-14 21:21:49 +0530

    first commit dark to light

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
           <div className="relative flex flex-col h-screen">
             <main className="container mx-auto w-full flex-grow">
               {children}
